0269749c1d3c0a1b93f528c29c9a9561c55b2ac6
Setu Biswas <setubiswas@setus-mbp.attlocal.net>
2017-10-15 18:41:20 -0500

    second draft

--- letter.js
+++ letter.js
@@ -1,9 +1,20 @@
-var letterConstructor = function(letter) {
-	this.letter = letter;
-	this.show = false;
-	this.createLetter = function() {
-		return this.show ? this.letter : "_";
-	}
-};
+// var letterConstructor = function(letter) {
+// 	this.letter = letter;
+// 	this.show = false;
+// 	this.createLetter = function() {
+// 		return this.show ? this.letter : "_";
+// 	}
+// };
 
-module.exports = letterConstructor;
+// module.exports = letterConstructor;
+
+var fs = require('fs');
+var word = require('./word.js');
+
+var Letters = function (word, letter) {
+	this.wordArr = [];
+
+	this.letter = function (word) {
+		this.wordArr.push(word);
+	}
+}
--- word.js
+++ word.js
@@ -1,38 +1,55 @@
-var letterConstructor = require("./letter.js");
+// var letterConstructor = require("./letter.js");
 
-// var word = "";
+// // var word = "";
 
-function WordConstructor(word) {
-	this.word = word;
-	this.letters = [];
-	this.getLetters = function() {
-		for (var i = 0; i < this.letters.length; i++) {
-			this.letters.push(new letterConstructor(this.letters[i]));
+// function WordConstructor(word) {
+// 	this.word = word;
+// 	this.letters = [];
+// 	this.getLetters = function() {
+// 		for (var i = 0; i < this.letters.length; i++) {
+// 			this.letters.push(new letterConstructor(this.letters[i]));
+// 		}
+// 		return this.letter.renderLetter;
+// 	}
+
+// 	this.checkLetter = function(userGuess) {
+// 		for (var i = this.letters.length - 1; i >= 0; i--) {
+// 			this.letters[i]
+// 			if (this.letters[i].letter = userGuess) {
+// 				this.letters[i].show = true;
+// 			}
+// 		}
+// 	}
+
+// 	this.renderWord = function() {
+// 		var string = "";
+// 		for (var i = 0; i < this.letters.length; i++) {
+// 			string += this.letters[i].renderLetter();
+// 		}
+// 		return string;
+// 	}
+// }
+
+// var currentWord = new WordConstructor ("Eagles");
+
+// var wordLetters = currentWord.getLetters();
+
+// console.log(word.renderWord());
+
+var fs = require('fs');
+wordBank = ['par', 'birdie'];
+
+var TheWord = function (word) {
+	this.word = wordBank[Math.floor(Math.random() * wordBank.length)];
+	console.log(this.word);
+	this.wordArr = [];
+		this.wordArr.push(this.word);
+		for (var i = 0; i < this.word.length; i++) {
+			this.wordArr[i] = "_";
 		}
-		return this.letter.renderLetter;
-	}
-
-	this.checkLetter = function(userGuess) {
-		for (var i = this.letters.length - 1; i >= 0; i--) {
-			this.letters[i]
-			if (this.letters[i].letter = userGuess) {
-				this.letters[i].show = true;
-			}
-		}
-	}
-
-	this.renderWord = function() {
-		var string = "";
-		for (var i = 0; i < this.letters.length; i++) {
-			string += this.letters[i].renderLetter();
-		}
-		return string;
-	}
+		console.log(this.wordArr);
 }
+TheWord();
+module.exports = TheWord;
 
-var currentWord = new WordConstructor ("Eagles");
-
-var wordLetters = currentWord.getLetters();
-
-console.log(word.renderWord());
 
