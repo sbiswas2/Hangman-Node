24d0b8bf65fb30e38acba76f40c277945f7130d5
Setu Biswas <setubiswas@setus-mbp.attlocal.net>
2017-10-15 19:37:59 -0500

    inquirer function

--- game.js
+++ game.js
@@ -1,8 +1,33 @@
 var inquirer = require('inquirer');
-var game = require("./word");
+var word = require("./word");
 var count = 0;
+var userGuess = [];
 
 // find word function?
 
 // enter letter function?
 	// recursion function where it loops until count of blank spaces = 0
+
+
+var Choice = function(word) {
+	if (count > 0) {
+		inquirer.prompt([{
+			name: 'question',
+			message: this.wordArr
+		}
+		]).then(function(answers) {
+			userGuess.push(answers);
+			console.log(userGuess);
+			if (userGuess === this.wordArr) {
+				console.log('correct');
+				console.log(count);
+			} else {
+				console.log('wrong');
+				count--;
+				console.log(count);
+			}
+		})
+	}
+}
+
+Choice();
