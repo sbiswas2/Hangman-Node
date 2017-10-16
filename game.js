ce9cbe6d1ce17fe0bb2920554fef35dadde70088
Setu Biswas <setubiswas@setus-mbp.attlocal.net>
2017-10-15 19:25:57 -0500

    changed functions

--- game.js
+++ game.js
@@ -1,1 +1,8 @@
+var inquirer = require('inquirer');
 var game = require("./word");
+var count = 0;
+
+// find word function?
+
+// enter letter function?
+	// recursion function where it loops until count of blank spaces = 0
